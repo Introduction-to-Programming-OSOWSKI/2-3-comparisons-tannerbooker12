0d903dd53fbab59ca2b3ed8efe10115df565c36f
tannerbooker12 <829075@mystma.org>
2022-09-20 09:33:14 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-#WRITE YOUR CODE IN THIS FILE
+def greatherThan(a,b):
+    if a>b :
+        return("true")
+    else:
+        return("false") 
+def lessThan(a,b):
+    if a<b:
+        return("true")
+    else: 
+        return("false")
+def equalTo(a,b):
+    if a==b:
+        return("true")
+    else:
+        return("false")
+def greaterOrEqual(a,b):
+    if a>=b:
+        return("true")
+    else:
+        return("false")
+def lessOrEqual(a,b):
+    if a<=b:
+        return("true")
+    else:
+        return("false")
